e56a4f8271a01840ed15da81787738aba8910c66
brian <brian.ogango@student.moringaschool.com>
2022-11-24 20:01:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+let activity =""
+
+function saturdayFun( activity ="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}const mondayWork = function ( activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(visualFlair="*") {
+   return function (talent = "special") {
+    return `You are ${visualFlair}${talent}${visualFlair}!`
+    
+   } 
+}
+console.log(wrapAdjective("||")("a dedicated programmer")("||")("!"))
